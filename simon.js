412702f14444910555dc6736164137f61122f365
Molly Kraus <mollyjkraus@gmail.com>
2016-03-14 22:09:23 -0400

    add comments to simon.js and bootstrap

--- simon.js
+++ simon.js
@@ -1,10 +1,11 @@
 "use strict";
+$(document).ready(function(){
 
-(function(){
 var beep = new Audio (); // found simple tutorial about audio on 
 beep.src = "beep-08b.mp3";
 
-$( '#start-btn' ).mousedown(function(){ //mouse down, mouse up function to change and run findColor();
+//click function for start button runs findColor();
+$( '#start-btn' ).mousedown(function(){
 	findColor();
 	$(this).css('background-color','#c59e9e');
 });
@@ -12,62 +13,52 @@ $( "#start-btn" ).mouseup(function() {
   $(this).css('background-color','#e9baba');
 });
 
-
-// var startButton = $( '#start-btn' ); //use this click function twice on two different buttons. I wrote a function so I could repeat the click
-// var resetButton = $( "#restart-btn" ); //need to pass different start and reset functions, at different times, so not sure how to call these	
-
-// var clickButton = function(button){
-// 	(button).mousedown(function(){
-// 	$(this).css('background-color','#c59e9e');
-// 	});
-// 	$(button).mouseup(function() {
-//    	$(this).css('background-color','#e9baba');
-//  });
-// }
-// clickButton(startButton);
-
+// arrays of color choices, game piece tiles, and empty array for random sequence of tiles
 var colorArray = ['purple','blue','green','red'];
 
 var colorTiles = [$('.purple'),$('.blue'),$('.green'),$('.red')];
 
 var newArray = [];
 
+// chooses random color from the four tile choices
 var findColor = function(){
 	var color = colorArray[Math.floor(Math.random() * colorArray.length)];
 	newArray.push(color);
-	$( '#start-btn' ).off('mousedown'); //turn off my start button when function begins, so it cannot be pushed
-	loopThroughColors();				//again and reactivated. 
+	$( '#start-btn' ).off('mousedown'); //turns off start button
+	loopThroughColors();
 }
 
+// function loop through array of random colors. Calls showColor
 function loopThroughColors() {
-	colorTiles.forEach(function(tile){ // turns off click button for simon game pieces
+	colorTiles.forEach(function(tile){ // turns off click function for tiles
 		tile.off('click');
 	})
-	newArray.forEach(function(target, index, arr){ //function loops through the array of random colors. It calls 
-		showColor(target, (index+1), arr);			//showColor to run. 
+	newArray.forEach(function(target, index, arr){
+		showColor(target, (index+1), arr);
 	});
 }
 
-//showColor displays and hides game pieces. On timer to delay the elements in the .forEach from 
+//showColor changes the opacity of the color tiles. The timer delays the elements in the .forEach from 
 //looping through too quickly. Outside timer is 500 milliseconds behind to allow delay between elements 
 //displaying. Add one to index number and multiply by 1500 milliseconds, so specific element runs at correct
 //timing. Ex: Index 1 runs at 1500 milliseconds. Index 2 runs at 3000 milliseconds.  
 
 
 function showColor(target,index,arr){ 
-	setTimeout(function(){				
-		$('#' + target).css('display','none');
-		beep.play();				//added beep function
+	setTimeout(function(){
+		$('.' + target).css('opacity', '0.5');
+		// beep function
+		beep.play();
 		setTimeout(function(){
-			$('#' + target).css('display','block');
+			$('.' + target).css('opacity','1');
 			if (index === arr.length){
-				colorTiles.forEach(function(tile,index){ //turn click function on add beep and display and hide game pieces.
+				colorTiles.forEach(function(tile,index){ //turn click function on add beep and display and hide tiles.
 					tile.click(function(){			//delay on click as well in order to display and hide pieces.
 						setTimeout(function(){
-							tile.css('display','none');
-							beep.play();	//added beep function
+							tile.css('opacity','0.5');
+							beep.play();
 						setTimeout(function(){
-								tile.css('display','block');
+							tile.css('opacity','1');
 						},100)
 						},100);
 						compare(colorArray[index]); //each click calls compare function. Adds string of 
@@ -79,11 +70,11 @@ function showColor(target,index,arr){
 	},index * 1500);
 }
 
-
 var x=0;
 
 var rounds = 0
-var compare = function(select){	//compares click to randomly generated color array. 
+//compares click to randomly generated color array.
+var compare = function(select){ 
 	if(newArray[x] !== select){
 		$('#gameOver').css('display', 'block'); //if not === then game over and reset button appear change color on click
 		$('#restart-btn').css('display', 'block'); //refreshes page when reset is clicked
@@ -100,17 +91,10 @@ var compare = function(select){	//compares click to randomly generated color arr
 					findColor();		//if correct, then continues to next round, resets x and calls findColor to 
 					x=0;		//push new randomly choosen color into array
 					rounds++;
-					$('div#score p').text(rounds);
+					$('div#score').text(rounds);
 
 				}
     		}
     	}
-
-
-
-$(document).ready(function(){
-
-
-})
 
 })();
